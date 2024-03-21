d28981299902faa761a573a0e0969e820d7b4e9a
Oscar Arley Sanchez <oa.sanchez2@uniandes.edu.co>
2024-03-20 23:09:06 -0500

    se incluye internacionalización

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,29 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes,withComponentInputBinding(), withPreloading(PreloadAllModules)),
-    provideHttpClient(),
-    provideClientHydration()]
+    provideClientHydration(),
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(TranslateModule.forRoot({
+      loader:{
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'ingles'
+    }))
+  ]
 };
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return  new  TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
+}
--- src/app/domains/entrenamientos/pages/nuevo-entrenamiento/nuevo-entrenamiento.component.ts
+++ src/app/domains/entrenamientos/pages/nuevo-entrenamiento/nuevo-entrenamiento.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { ToastrService } from 'ngx-toastr'
 
 @Component({
   selector: 'app-nuevo-entrenamiento',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './nuevo-entrenamiento.component.html',
   styleUrl: './nuevo-entrenamiento.component.css'
 })
-export class NuevoEntrenamientoComponent {
+export class NuevoEntrenamientoComponent implements OnInit  {
+  companyForm = this.formBuilder.group({
+    name: ['', [Validators.required, this.alphanumeric]],
+    email: ['', [Validators.required, Validators.email]],
+    address: ['', [Validators.required, this.alphanumeric]],
+    country: ['', [Validators.required]],
+    dept: ['', [Validators.required]],
+    city: ['', [Validators.required, this.alphabetical]],
+    phone: ['', [Validators.required, this.numeric]],
+    contact_name: ['', [Validators.required, this.alphabetical]],
+    contact_phone: ['', [Validators.required, this.numeric]],
+  });
+
+  bottonDisable:Boolean = true;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastr:ToastrService
+  ) {
+  }
+
+  ngOnInit() {
+   
+  }
+
+  
+  alphanumeric(control: FormControl) {
+    const alphanumericRegex = /^[a-zA-Z0-9\s]*$/;
+    const value = control.value;
+    const isValid = alphanumericRegex.test(value);
+    return isValid ? null : { 'alphanumeric': true };
+  }
+
+  alphabetical(control: FormControl) {
+    const alphabeticalRegex = /^[a-zA-Z ]*$/;
+    const value = control.value;
+    const isValid = alphabeticalRegex.test(value);
+    return isValid ? null : { 'alphabetical': true };
+  }
+
+  numeric(control: FormControl) {
+    const numericRegex = /^[0-9]*$/;
+    const value = control.value;
+    const isValid = numericRegex.test(value);
+    return isValid ? null : { 'numeric': true };
+  }
 
 }
