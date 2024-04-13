3c776e78a82547735b198067754a8c624f44b643
Oscar Arley Sanchez <oa.sanchez2@uniandes.edu.co>
2024-04-13 10:52:53 -0500

    Configuración endpoints aws

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,10 @@
+/*
 export const environment = {
     URL_EJERCICIOS: "http://127.0.0.1:5000/ejercicios",
     URL_ENTRENAMIENTO: "http://127.0.0.1:5002/entrenamientos"
+};
+*/
+export const environment = {
+    URL_EJERCICIOS: "http://a1b7563a0858f4f71b48c713a5de8c20-991979942.us-east-1.elb.amazonaws.com/ejercicios",
+    URL_ENTRENAMIENTO: "http://a1b7563a0858f4f71b48c713a5de8c20-991979942.us-east-1.elb.amazonaws.com/entrenamientos"
 };
