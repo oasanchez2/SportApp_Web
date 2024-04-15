ccac81fc8d1913e81ad0d7d06081690c88ace9f6
Paola_Forero <p.forerob@uniandes.edu.co>
2024-04-14 19:41:01 -0500

    Se realiza finalización visualización del calendario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { NuevoEntrenamientoComponent } from './domains/entrenamientos/pages/nuev
 import { LoginComponent } from './domains/seguridad/pages/login/login.component';
 import { NotFoundComponent } from './domains/shared/pages/not-found/not-found.component';
 import { CalendarioEventosComponent } from './domains/calendario/pages/calendario-eventos/calendario-eventos.component';
+import { HeaderComponent} from './domains/shared/components/header/header.component'
 export const routes: Routes = [
     {
         path: '',
-        component: NuevoEntrenamientoComponent,        
+        component: NuevoEntrenamientoComponent,
     },
     {
         path:'login',
@@ -15,6 +16,10 @@ export const routes: Routes = [
     {
         path:'calendario',
         component: CalendarioEventosComponent
+    },
+    {
+      path:'header',
+      component: HeaderComponent
     },
     {
         path: '**',
