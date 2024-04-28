6a3ccf61ed47a3625cdf64fcd2d0819fe1403e97
Oscar Arley Sanchez <oa.sanchez2@uniandes.edu.co>
2024-04-28 15:18:54 -0500

    version 1.05

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    VERSION: "v.1.0.4",
+    VERSION: "v.1.0.5",
     URL_EJERCICIOS: "http://a1b7563a0858f4f71b48c713a5de8c20-991979942.us-east-1.elb.amazonaws.com/ejercicios",
     URL_ENTRENAMIENTO: "http://a1b7563a0858f4f71b48c713a5de8c20-991979942.us-east-1.elb.amazonaws.com/entrenamientos",
     URL_EVENTOS: "http://a1b7563a0858f4f71b48c713a5de8c20-991979942.us-east-1.elb.amazonaws.com/eventos",
