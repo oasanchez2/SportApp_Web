47fd143a47c471dfa019398753ba5868de2204f8
Oscar Arley Sanchez <oa.sanchez2@uniandes.edu.co>
2024-05-10 20:01:14 -0500

    ajuste nav

--- src/app/domains/shared/components/layout/layout.component.ts
+++ src/app/domains/shared/components/layout/layout.component.ts
@@ -3,11 +3,12 @@ import { CommonModule} from '@angular/common';
 import { HeaderComponent } from './../../../shared/components/header/header.component'
 import { FooterComponent } from './../../../shared/components/footer/footer.component'
 import { RouterModule } from '@angular/router';
+import { NavInvitadoComponent } from '../../../shared/components/nav-invitado/nav-invitado.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule,RouterModule,HeaderComponent,FooterComponent],
+  imports: [CommonModule,RouterModule,HeaderComponent,FooterComponent,NavInvitadoComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
